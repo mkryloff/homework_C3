9c42a53332ad034f5faf6b19ff715116d596e5b0
kryloff <matwej71@gmail.com>
2022-09-11 17:50:59 +0300

    Finish

---
+++ Program1.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+if((number[0] == number[4]) || (number[1] == number[3]))
+{
+    System.Console.WriteLine("Ваше число палиндром");
+}
+else
+{
+    System.Console.WriteLine("Ваше число не палиндром");
+}
---
+++ Program3.cs
@@ -0,0 +1,6 @@
+﻿System.Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= number; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3));
+}
